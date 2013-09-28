b5e61c42295b39d777466c014cb4508bf4186c07
Grarak <williye97@gmail.com>
2013-09-28 19:35:33 +0200

    error when names are empty
    Signed-off-by: Grarak <williye97@gmail.com>

--- src/com/grarak/romswitcher/SetNameActivity.java
+++ src/com/grarak/romswitcher/SetNameActivity.java
@@ -9,6 +9,8 @@
 
 package com.grarak.romswitcher;
 
+import com.grarak.romswitcher.Utils.Utils;
+
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -35,17 +37,21 @@ public class SetNameActivity extends Activity {
 	    mButtonNext.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				SharedPreferences FIRST_NAME = getSharedPreferences(PREF_NAME_FIRST, 0);
-				SharedPreferences SECOND_NAME = getSharedPreferences(PREF_NAME_SECOND, 0);
-				
-				SharedPreferences.Editor eFirstname = FIRST_NAME.edit();
-				SharedPreferences.Editor eSecondname = SECOND_NAME.edit();
-				
-				eFirstname.putString("firstname", mFirstEdit.getText().toString());
-				eSecondname.putString("secondname", mSecondEdit.getText().toString());
-				
-				eFirstname.commit();
-				eSecondname.commit();
+				if (mFirstEdit.getText().toString().isEmpty() || mSecondEdit.getText().toString().isEmpty()) {
+					Utils.toast(SetNameActivity.this, getString(R.string.emptynames), 0);
+				} else {
+					SharedPreferences FIRST_NAME = getSharedPreferences(PREF_NAME_FIRST, 0);
+					SharedPreferences SECOND_NAME = getSharedPreferences(PREF_NAME_SECOND, 0);
+					
+					SharedPreferences.Editor eFirstname = FIRST_NAME.edit();
+					SharedPreferences.Editor eSecondname = SECOND_NAME.edit();
+					
+					eFirstname.putString("firstname", mFirstEdit.getText().toString());
+					eSecondname.putString("secondname", mSecondEdit.getText().toString());
+					
+					eFirstname.commit();
+					eSecondname.commit();
+				}
 			}
 		});
 	}
